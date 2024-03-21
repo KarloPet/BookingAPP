06ea2538c21a9041daa9196cb199b8347a01e06a
Karlo Peterfaj <karlo.peterfaj@codelab-software.hr>
2024-03-21 19:53:02 +0100

    update

--- Frontend/src/pages/Cjenik/Cjenik.jsx
+++ Frontend/src/pages/Cjenik/Cjenik.jsx
@@ -1,88 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Table, Button } from 'react-bootstrap';
-import { FaEdit, FaTrash } from "react-icons/fa";
-import CjenikService from '../../services/CjenikService';
-import "./Cjenik.css";
+import { Container, Button, Modal } from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import { useNavigate } from 'react-router-dom';
-import { RoutesNames } from '../../constants';
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { useAuth } from '../../AuthContext';
+import { RoutesNames } from '../../constants';
+//import {CjenikService} from '../../services/CjenikService'
 
 function Cjenik() {
     const { currentUser } = useAuth();
     const navigate = useNavigate();
     const [cjenik, setCjenik] = useState([]);
-    const [odabranaGodina, setOdabranaGodina] = useState(new Date().getFullYear());
-    const godine = Array.from({ length: 5 }, (_, index) => 2023 + index);
+    const [activeDate, setActiveDate] = useState(new Date());
+    const [showModal, setShowModal] = useState(false);
+    const [selectedCijena, setSelectedCijena] = useState(null);
 
-    const fetchCjenikByMonthAndYear = async (year, month) => {
+    useEffect(() => {
+        fetchCjenik(activeDate.getFullYear(), activeDate.getMonth() + 1);
+    }, [activeDate]);
+
+    const fetchCjenik = async (year, month) => {
         try {
             const response = await axios.get(`https://karlopet-001-site1.ktempurl.com/api/v1/Cjenik/ByMonth?year=${year}&month=${month}`);
-            setCjenik(response.data.sort((a, b) => new Date(a.datum) - new Date(b.datum)));
+            setCjenik(response.data);
         } catch (error) {
             console.error('Error fetching cjenik:', error);
         }
     };
 
-    const months = ["Siječanj", "Veljača", "Ožujak", "Travanj", "Svibanj", "Lipanj", "Srpanj", "Kolovoz", "Rujan", "Listopad", "Studeni", "Prosinac"];
+    const handleDayClick = (value) => {
+        const datumString = `${value.getFullYear()}-${(value.getMonth() + 1).toString().padStart(2, '0')}-${value.getDate().toString().padStart(2, '0')}`;
+        const foundCijena = cjenik.find(d => d.datum.startsWith(datumString));
+        if (foundCijena) {
+            setSelectedCijena(foundCijena);
+            setShowModal(true);
+        }
+    };
+
+    const closeModal = () => setShowModal(false);
+
+    const tileContent = ({ date, view }) => {
+        if (view === 'month') {
+            const datumString = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+            const cijena = cjenik.find(d => d.datum.startsWith(datumString))?.cijena;
+            return cijena ? <p>{`${cijena} kn`}</p> : null;
+        }
+    };
 
     return (
         <Container>
-            <h2 className='naslov2'>CJENIK</h2>
             {currentUser && currentUser.permissionLevel === 'admin' && (
-                <Button className='btn btn-success dugmeDodaj' onClick={() => navigate(RoutesNames.CJENIKDODAJSVE)}>
+                <Button className='btn btn-success' onClick={() => navigate(RoutesNames.CJENIKDODAJSVE)}>
                     DODAJ NOVU CIJENU
                 </Button>
             )}
+            <Calendar
+                onChange={setActiveDate}
+                value={activeDate}
+                onClickDay={handleDayClick}
+                tileContent={tileContent}
+                locale="hr-HR"
+            />
 
-            <h2 className='naslov3'>Dohvati cjenik po godini i mjesecu</h2>
+            <Modal show={showModal} onHide={closeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Uredi Cijenu</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Datum: {selectedCijena ? new Date(selectedCijena.datum).toLocaleDateString() : ''}
+                    <br />
+                    Cijena: {selectedCijena?.cijena} kn
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={closeModal}>
+                        Zatvori
+                    </Button>
+                    <Button variant="primary" onClick={() => navigate(RoutesNames.CJENIKDODAJ, { state: selectedCijena })}>
+                        <FaEdit size={20} /> Uredi
+                    </Button>
 
-            <div className="year-selection button-container" style={{ marginBottom: '20px' }}>
-                {godine.map((godina) => (
-                    <Button key={godina} variant={odabranaGodina === godina ? "secondary" : "outline-secondary"} onClick={() => { setOdabranaGodina(godina); setOdabraniMjesec(null); }}>
-                        {godina}
-                    </Button>
-                ))}
-            </div>
-            <div className="month-selection button-container" style={{ marginBottom: '20px' }}>
-                {months.map((month, index) => (
-                    <Button key={index} variant="primary" onClick={() => fetchCjenikByMonthAndYear(odabranaGodina, index + 1)} style={{ margin: '5px' }}>
-                        {month}
-                    </Button>
-                ))}
-            </div>
-            <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        <th style={{ width: currentUser && currentUser.permissionLevel === 'admin' ? '40%' : '50%' }}>Datum</th>
-                        <th style={{ width: currentUser && currentUser.permissionLevel === 'admin' ? '40%' : '50%' }}>Cijena</th>
-                        {currentUser && currentUser.permissionLevel === 'admin' && (
-                            <th style={{ width: '20%' }}>Akcija</th>
-                        )}
-                    </tr>
-                </thead>
-                <tbody>
-                    {cjenik.map((item) => (
-                        <tr key={item.id}>
-                            <td>{new Date(item.datum).toLocaleDateString('hr-HR')}</td>
-                            <td>{item.cijena} kn</td>
-                            {currentUser && currentUser.permissionLevel === 'admin' && (
-                                <td>
-                                    <Button variant="primary" onClick={() => navigate(RoutesNames.CJENIKDODAJ, { state: { id: item.id, datum: item.datum, cijena: item.cijena } })}>
-                                        <FaEdit size={20} />
-                                    </Button>
-                                    &nbsp;&nbsp;&nbsp;
-                                    <Button variant="danger" onClick={() => CjenikService.obrisiCjenu(item.id).then(() => fetchCjenikByMonthAndYear(odabranaGodina, new Date(item.datum).getMonth() + 1))}>
+
+                    <Button variant="danger" onClick={() => CjenikService.obrisiCjenu(item.id)}>
                                         <FaTrash size={20} />
                                     </Button>
-                                </td>
-                            )}
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+
+
+
+
+                    {/* <Button variant="danger" onClick={() => {
+                        // Ovdje implementirajte logiku brisanja
+                        closeModal();
+                    }}>
+                        <FaTrash size={20} /> Obriši
+                    </Button> */}
+                </Modal.Footer>
+            </Modal>
         </Container>
     );
+
 }
 
 export default Cjenik;
